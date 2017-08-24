9950db0dc4377c9307287f8b740e8be95feb87b5
Jesse Davila <jessedavila3@gmail.com>
2017-08-24 09:55:16 -0500

    fixed innerHTML error from /h2 to /h1

--- cat_clicker.js
+++ cat_clicker.js
@@ -5,7 +5,7 @@ var counter = [0, 0];
 
 var catInit = function(i) {
     var children = catPics[i].children;
-    children[0].innerHTML = "<h1>" + cats[i] + "</h2>";
+    children[0].innerHTML = "<h1>" + cats[i] + "</h1>";
     children[2].innerHTML = counter[i];
     catPics[i].addEventListener('click', function() {
         this.children[2].innerHTML = ++counter[i];
