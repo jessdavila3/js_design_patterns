8469ced305485ab21eb49650df09cc3b1cae9287
Jesse Davila <jessedavila3@gmail.com>
2017-08-24 00:10:51 -0500

    initial commit, cat_clickerv2 now has two cats

---
+++ cat_clicker.js
@@ -0,0 +1,20 @@
+(function() {
+var catPics = document.getElementsByClassName("catPic");
+var cats = ["Rocky", "Apollo"];
+var counter = [0, 0];
+
+var catInit = function(i) {
+    var children = catPics[i].children;
+    children[0].innerHTML = "<h1>" + cats[i] + "</h2>";
+    children[2].innerHTML = counter[i];
+    catPics[i].addEventListener('click', function() {
+        this.children[2].innerHTML = ++counter[i];
+        console.log(i);
+    })
+}
+
+for(var i = 0; i < catPics.length; i++) {
+    catInit(i);
+}
+
+} ());
