49cb34eaf1adb0ba8d6004a9ce098a5a83ab5e3c
Jesse Davila <jessedavila3@gmail.com>
2017-09-07 22:37:42 -0500

    mvc approach

--- cat_clicker.js
+++ cat_clicker.js
@@ -3,17 +3,36 @@ var catPics = document.getElementsByClassName("catPic");
 var cats = ["Rocky", "Apollo", "Lola", "Bear"];
 var counter = [0, 0, 0, 0];
 
-var catInit = function(i) {
-    var children = catPics[i].children;
-    children[0].innerHTML = "<h1>" + cats[i] + "</h1>";
-    children[2].innerHTML = counter[i];
-    catPics[i].addEventListener('click', function() {
-        this.children[2].innerHTML = ++counter[i];
-    })
+var model = {
+    
 }
 
-for(var i = 0; i < catPics.length; i++) {
-    catInit(i);
+var view = {
+    init: function() {
+        var catTitle = document.getElementById("catTitle");
+        var catPicture = document.getElementById("catPicture");
+        var catCounter = document.getElementById("catCounter");
+    }
 }
 
+var controller = {
+    init: function() {
+        model.init();
+        view.init();
+    },
+    
+}
+// var catInit = function(i) {
+//     var children = catPics[i].children;
+//     children[0].innerHTML = "<h1>" + cats[i] + "</h1>";
+//     children[2].innerHTML = counter[i];
+//     catPics[i].addEventListener('click', function() {
+//         this.children[2].innerHTML = ++counter[i];
+//     })
+// }
+
+// for(var i = 0; i < catPics.length; i++) {
+//     catInit(i);
+// }
+controller.init();
 } ());
