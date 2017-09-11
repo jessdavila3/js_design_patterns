36f8e8ee5a852a9ea44de012bc2d2d99040bbe88
Jesse Davila <jessedavila3@gmail.com>
2017-09-11 14:02:34 -0500

    changed src for img, added stylesheet, built out init functions

--- cat_clicker.js
+++ cat_clicker.js
@@ -1,66 +1,66 @@
 (function() {
-    
+
 var model = {
+    selectedCat: null,
     cats: [{
         name: "Rocky",
-        img: "cat.jpg",
+        img: "img/cat.jpg",
         counter: 0
     },{
         name: "Monster",
-        img: "cat2.jpg",
+        img: "img/cat2.jpg",
         counter: 0
     },{
         name: "Max",
-        img: "cat3.jpg",
+        img: "img/cat3.jpg",
         counter: 0
     },{
         name: "Bosco",
-        img: "cat4.jpg",
+        img: "img/cat4.jpg",
         counter: 0
     }],
 }
 
-var view = {
+var catView = {
     init: function() {
         var catTitle = document.getElementById("catTitle");
         var catPicture = document.getElementById("catPicture");
         var catCounter = document.getElementById("catCounter");
+        this.render();
     },
-    initList: function() {
+    render: function() {
+        var currentCat = controller.getSelectedCat();
+        console.log(currentCat);
+        catTitle.innerHTML = currentCat.name;
+        catPicture.src = currentCat.img;
+        catCounter.innerHTML = currentCat.counter;
+    }
+}
+
+var catListView = {
+    init: function () {
         this.catList = document.getElementById("catList");
         var htmlStr = '';
-        model.cats.forEach(function(cat) {
+        model.cats.forEach(function (cat) {
             console.log(cat.name + " " + cat.counter + " " + cat.img);
-            htmlStr += '<li><button>' +
-            cat.name + 
-            '</button></li>';
+            htmlStr += '<li><button class="cat-button">' +
+                cat.name +
+                '</button></li>';
         })
         this.catList.innerHTML = htmlStr;
     },
-    renderList: function() {
-        var htmlStr = '';
-    }
 }
 
 var controller = {
     init: function() {
-        // model.init();
-        view.init();
-        view.initList();
+        model.selectedCat = model.cats[0];
+        catView.init();
+        catListView.init();
     },
+    getSelectedCat: function() {
+        return model.selectedCat;
+    }
     
 }
-// var catInit = function(i) {
-//     var children = catPics[i].children;
-//     children[0].innerHTML = "<h1>" + cats[i] + "</h1>";
-//     children[2].innerHTML = counter[i];
-//     catPics[i].addEventListener('click', function() {
-//         this.children[2].innerHTML = ++counter[i];
-//     })
-// }
-
-// for(var i = 0; i < catPics.length; i++) {
-//     catInit(i);
-// }
 controller.init();
 } ());
